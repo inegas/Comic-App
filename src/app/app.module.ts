78f9772a857f835b9e92ecfef440e49c3de21e62
Angel Banegas <mcinegas@gail.com>
2019-02-25 11:39:23 +0100

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Servicios
 
+
+
+
+// Rutas
+import { app_routing } from './app/app.routes';
+
+
+
+
+
+// Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './app/components/shared/navbar/navbar.component';
+import { HomeComponent } from './app/components/home/home.component';
+import { AboutComponent } from './app/components/about/about.component';
+import { HeroesComponent } from './app/components/heroes/heroes.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app/services/heroes.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroesService {
+
+  constructor() {
+    console.log('servicio listo para usarse!!');
+    
+   }
+
+}
